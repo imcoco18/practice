c258aebf1d51404ee80657eb2f57242bed332ae4
imcoco18 <coco.jasmine18@icloud.com>
2020-08-16 10:57:56 +0900

    [add]day1-day9 pagetop modal

--- day1-day9/js/practice.js
+++ day1-day9/js/practice.js
@@ -4,3 +4,33 @@ $(function() {
     $('#menu-nav').show();
   });
 });
+
+$(function() {
+  var pagetop = $('#page_top');
+  pagetop.hide();
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      pagetop.fadeIn();
+    } else {
+      pagetop.fadeOut();
+    }
+  });
+  pagetop.click(function() {
+    $('body, html').animate({ scrollTop:0 }, 500);
+    return false;
+  });
+});
+
+$('.course-item a').click(function() {
+  var imgSrc = $(this).children().attr('src');
+  $('.bigimg').children().attr('src', imgSrc);
+  $('.modal').fadeIn();
+  $('body, html').css('overflow-y', 'hidden');
+  return false
+});
+$('.close-btn').click(function() {
+  $('.modal').fadeOut();
+  $('body, html').css('overflow-y', 'visible');
+  return false
+});
