5df9c2b3e4a25ccf5d591869d86247b3b108067a
imcoco18 <coco.jasmine18@icloud.com>
2020-09-15 18:40:01 +0900

    [add]feature

--- Uniwa/js/practice.js
+++ Uniwa/js/practice.js
@@ -5,3 +5,19 @@
 //   function(){
 
 // });
+
+$(function(){
+  $('.feature-box img').hover(
+    function(){
+      $(this).animate({
+        'width':'400px'
+      },300);
+    },
+    function(){
+      $(this).animate({
+        'width':'350px'
+      },300);
+    }
+    );
+
+});
